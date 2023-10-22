62421f252cbb01696bbda2061a3c331cb5c2870e
spirit-124 <bajrangofficial580@gmail.com>
2023-10-22 17:54:36 +0530

    Initial Commit

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (<div>
+
+   <Button variant="outline" size="lg">Click Me</Button>
+  </div>
+  )
+}
